8389ff75eb34b1eb1da90d1cfcb58bbc0785c3e3
Mehul Ved <mehul.n.ved@colearn.id>
2022-05-02 10:26:24 +0530

    Fix config issues

--- fill_certificate.py
+++ fill_certificate.py
@@ -8,42 +8,43 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-image_width = config['image']['width']
 with open('data/timesheet.csv') as csvfile:
     tsreader = csv.DictReader(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     for row in tsreader:
         img = Image.open("certificate-template.jpg")
         draw = ImageDraw.Draw(img)
-        name = row['name']
-        distance = row['distance']
-        time = row['time']
         # Credits:
         # https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
         # Prints Name
         # name = 'Warnakulasuriya Patabendige Ushantha Joseph Chaminda Vaas'
-        W, H = (image_width,height)
-        w, h = font.getsize(name)
+        image_width = int(config['image']['width'])
+        image_height = int(config['image']['height'])
+        W, H = (image_width, image_height)
 
         # Prints Name
-        name_width = config['name']['width_offset_left'] + (W-w)/2 - config['name']['width_offset_right']
-        name_height = config['name']['height']
-        name_font_size = config['name']['font_size']
+        name = row['name']
+        name_font_size = int(config['name']['font_size'])
         font = ImageFont.truetype(r'./news-serif.ttf', name_font_size)
-        draw.text(name_width, name_height), name, (0,0,0), font=font)
+        w, h = font.getsize(name)
+        name_width = int(config['name']['width_offset_left']) + int((W-w)/2) - int(config['name']['width_offset_right'])
+        name_height = int(config['name']['height'])
+        draw.text((name_width, name_height), name, (0,0,0), font=font)
 
         # Prints Distance
-        distance_width = config['distance']['width']
-        distance_height = config['distance']['height']
-        distance_font_size = config['distance']['font_size']
+        distance = row['distance']
+        distance_width = int(config['distance']['width'])
+        distance_height = int(config['distance']['height'])
+        distance_font_size = int(config['distance']['font_size'])
         font = ImageFont.truetype(r'./news-serif.ttf', distance_font_size)
         draw.text((distance_width, distance_height), distance, (0,0,0), font=font)
 
         # Prints Time
-        time_width = config['time']['width']
-        time_height = config['time']['height']
-        time_font_size = config['time']['font_size']
+        time = row['time']
+        time_width = int(config['time']['width'])
+        time_height = int(config['time']['height'])
+        time_font_size = int(config['time']['font_size'])
         font = ImageFont.truetype(r'./news-serif.ttf', time_font_size)
-        draw.text((time_width, time_height), time, (0,0,0), font=font)
+        draw.text((time_width, time_height), time, (0, 0, 0), font=font)
 
         img.save('certs/{0}.jpg'.format(name))
         img.close
